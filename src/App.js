a6a4a8a37e159218775f781efb2813e841571d04
gopi-anan <Gopisenthil2001@gmail.comm>
2022-11-28 14:02:57 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      first Name
+      <input type="First name"></input>
+      <br/>
+      last name
+      <input type="last name"></input>
+    </div>
+  );
+}
+
+export default App;
